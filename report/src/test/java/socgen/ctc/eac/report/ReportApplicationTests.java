b532c73d89cc582063e384d750bcaaf832775cd9
Kartike Chopra <62751693+Scythektk@users.noreply.github.com>
2024-06-26 23:26:01 +0530

    Add files via upload

---
+++ report/src/test/java/socgen/ctc/eac/report/ReportApplicationTests.java
@@ -0,0 +1,13 @@
+package socgen.ctc.eac.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
